c8430676b660c42762eb4d9b74bcff22fae085e6
mahi <mohiddinoshim@yahoo.com>
2020-11-15 23:05:47 +0600

    exe Done

---
+++ 08/application.rb
@@ -0,0 +1,57 @@
+class Application
+  attr_accessor :environment
+
+  def initialize
+    @environment = :development
+  end
+
+  def connect_to_database
+    puts "Connecting to #{@environment} database..."
+  end
+
+  def handle_request
+    puts "Handling #{@environment} request..."
+  end
+
+  def write_to_log
+    puts "Writing to #{@environment} log file..."
+  end
+
+def in_production
+  old_environment = @environment
+  @environment = :production
+  yield
+  rescue Exception => e
+    puts e.message
+  ensure
+    @environment = old_environment
+    puts "Reset environment to #{@environment}"
+end
+
+ 
+  # rescue Exception => e
+  #   puts e.message
+  # ensure
+  #   @environment = old_environment
+  #   puts "Reset environment to #{@environment}"
+
+end
+
+app = Application.new
+
+# app.environment = :production
+# app.connect_to_database
+# app.handle_request
+# app.write_to_log
+# app.environment = :development
+
+# app.connect_to_database
+# app.handle_request
+# app.write_to_log
+
+app.in_production do
+  app.connect_to_database
+  app.handle_request
+  raise "Boom!"
+  app.write_to_log
+end
